2d773788ea8e7376857bbec291fa8ef09c7b68ca
evenmoregarlic <evenmoregarlic@gmail.com>
2020-08-03 14:41:09 +0100

    Update fileTransformer.js

--- fileTransformer.js
+++ fileTransformer.js
@@ -36,7 +36,7 @@ async function convertFile(filename) {
     
     // more options to set, need to learn what is available
     video.setVideoFormat('mp4');
-    video.setVideoFrameRate(30);
+    /*video.setVideoFrameRate(30);*/
 
     console.debug(video.info_configuration);
 
