3d79a13cd12c260316d7dfe664573e94f6114f56
evenmoregarlic <evenmoregarlic@gmail.com>
2020-08-03 13:44:01 +0100

    Update recorder.js

--- public/recorder.js
+++ public/recorder.js
@@ -16,6 +16,8 @@ var recorderStoppedEvent = new Event('recorder-stopped');
  * Mime types, should be in order of preference.
  */
 var types = [
+	"video/webm\;codecs=vp9";
+	"video/webm\;codecs=vp8";
     "video/webm\;codecs=h264",
     "video/webm",
     "video/mpeg",
