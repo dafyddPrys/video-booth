2df86817103acfb06a799bf1f5d7f641cca00a2b
evenmoregarlic <evenmoregarlic@gmail.com>
2020-08-04 10:01:27 +0100

    Bitrate and codec changes
    Target bitrate for camera and mp4 output. Audio delay in ffmpeg. Using vp9 codec for original recording.

--- public/recorder.js
+++ public/recorder.js
@@ -46,7 +46,7 @@ ipcRenderer.on('download-reply', handleUploadReply);
 function initRecorder(stream) {
     const firstCompatibleMimeType = types.find(t => MediaRecorder.isTypeSupported(t))
     console.log(`using mime type ${firstCompatibleMimeType}`);
-	var options = { mimeType: firstCompatibleMimeType , videoBitsPerSecond: 10000000, audioBitsPerSecond: 48000};
+	var options = { mimeType: firstCompatibleMimeType , videoBitsPerSecond: 5000000, audioBitsPerSecond: 48000};
 
     recorder = new MediaRecorder(stream, options)
     recorder.ondataavailable = handleDataAvailable;
