32d26a9b989227ba0c56068f090cbd9ab59ff65a
Daf <daf.p.thomas@gmail.com>
2020-08-17 15:10:48 +0100

    set audio delay to 650, set resolution to 432x768

--- public/mediaSelector.js
+++ public/mediaSelector.js
@@ -67,7 +67,7 @@ function start() {
   const constraints = {
     audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
     video: {deviceId: videoSource ? {exact: videoSource} : undefined},
-	video: {width: 1280, height: 720 }
+	video: {width: 768, height: 432 }
   };
   navigator.mediaDevices.getUserMedia(constraints)
     .then(initRecorder)
